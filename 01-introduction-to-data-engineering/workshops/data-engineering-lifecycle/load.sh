aa8a1e714485795a84f87767f6894cf232485d2a
oakkpsw <patsawut_t@cmu.ac.th>
2023-04-22 10:21:25 +0000

    commmit on workshop 1

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+#parntry ID a172d577-a967-4fbe-906a-ec88153f1abc
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/a172d577-a967-4fbe-906a-ec88153f1abc/basket/randomDogs"
