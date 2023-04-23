2768653b808aa8bc49db6e3a04397485f590ce91
oakkpsw <patsawut_t@cmu.ac.th>
2023-04-23 02:32:18 +0000

    add main-postgres and some config

--- 00-bootcamp-project/main-sftp.py
+++ 00-bootcamp-project/main-sftp.py
@@ -1,5 +1,4 @@
 import configparser
-
 import pysftp
 
 
@@ -9,14 +8,19 @@ username = parser.get("sftp_config", "username")
 password = parser.get("sftp_config", "password")
 host = parser.get("sftp_config", "host")
 port = parser.getint("sftp_config", "port")
-
+DATA_FOLDER = "data"
 
 # Security risk! Don't do this on production
 # You lose a protection against Man-in-the-middle attacks
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
-files = ["addresses.csv","events.csv","order_items.csv","orders.csv","products.csv","promos.csv","users.csv"]
+files = [
+    "products.csv",
+    "promos.csv",
+]
 with pysftp.Connection(host, username=username, password=password, port=port, cnopts=cnopts) as sftp:
-    sftp.get("promos.csv")
+    for f in files:
+        sftp.get(f, f"{DATA_FOLDER}/{f}")
+        print(f"Finished downloading: {f}")
     #to-do get all file in servers
     # sftp.put("some.txt", "uploaded.txt")
