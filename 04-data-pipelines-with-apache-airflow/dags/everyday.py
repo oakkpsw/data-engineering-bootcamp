d9001d1110515213d755da4d76f368f75900c0a1
oakkpsw <patsawut_t@cmu.ac.th>
2023-05-13 09:22:46 +0000

    workshop day 4

---
+++ 04-data-pipelines-with-apache-airflow/dags/everyday.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+
+def _world():
+    print("world")
+
+with DAG(
+    dag_id="everyday", # must same with file name
+    schedule="@daily",
+    start_date=timezone.datetime(2023, 5, 1),
+    tags=["DEB","Skooldio"],
+    catchup=False, # ไม่ต้องรันย้อนหลัง
+):
+    
+    
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo 'hello'",
+        )
+    world = PythonOperator(
+        task_id="world",
+        python_callable=_world,
+
+    )
+    
+    hello >> world
---
+++ 04-data-pipelines-with-apache-airflow/dags/everymonday.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+
+def _world():
+    print("world")
+
+with DAG(
+    dag_id="everymonday", # must same with file name
+    schedule="0 0 * * 1",
+    start_date=timezone.datetime(2023, 5, 1),
+    tags=["DEB","Skooldio"],
+    catchup=False, # ไม่ต้องรันย้อนหลัง
+):
+    
+    
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo 'hello'",
+        )
+    world = PythonOperator(
+        task_id="world",
+        python_callable=_world,
+
+    )
+    
+    hello >> world
